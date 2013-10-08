ce1237826a8c7e8d26f6e7a5312164363ef22aef
jaroslaw.palka <none@none>
2013-10-09 01:06:57 +0200

    examples of blocking and non blocking api

---
+++ io/src/main/java/pl/symentis/jvminternals/io/BIOEchoServer.java
@@ -0,0 +1,56 @@
+package pl.symentis.jvminternals.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BIOEchoServer {
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(BIOEchoServer.class);
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = new ServerSocket(8888);
+
+		while (true) {
+			final Socket socket = serverSocket.accept();
+
+			LOGGER.debug("new connection");
+
+			Runnable connection = new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						BufferedReader reader = new BufferedReader(
+								new InputStreamReader(socket.getInputStream()));
+						String line = reader.readLine();
+						PrintWriter writer = new PrintWriter(
+								socket.getOutputStream());
+						writer.print(line);
+						writer.flush();
+
+					} catch (IOException e) {
+						LOGGER.error("", e);
+					} finally {
+						try {
+							socket.close();
+						} catch (IOException e) {
+							LOGGER.warn("", e);
+						}
+					}
+				}
+			};
+
+			new Thread(connection).start();
+
+		}
+	}
+}
