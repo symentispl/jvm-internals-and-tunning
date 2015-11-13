1b40b980c00009fcc025d268077d932d56b3f1bd
jaroslaw.palka <none@none>
2015-11-13 08:56:51 +0100

    part of refactoring done, need to add diagrams
    --HG--
    branch : asciidoc_refactor

--- authoring_slides.py
+++ authoring_slides.py
@@ -2,11 +2,10 @@
 from livereload import Server, shell
 import sh
 
-
-
 sh.mkdir("-p","target/slides")
 sh.cp("-R","--remove-destination","reveal.js","target/slides")
 
 server = Server()
+
 server.watch('src/main/asciidoc/*.adoc', shell('asciidoctor -T asciidoctor-reveal.js/templates/slim/ -r asciidoctor-diagram src/main/asciidoc/*.adoc -D target/slides/'))
 server.serve(root='target/slides')
