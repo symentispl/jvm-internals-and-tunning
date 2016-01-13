6c737b88cb86cdceb563b4662d3a0afa8d506bfe
jaroslaw.palka <none@none>
2016-01-13 13:35:28 +0100

    added example of uncommon trap in JIT

---
+++ jit/src/main/java/pl/symentis/jit/UncommonTrap.java
@@ -0,0 +1,21 @@
+package pl.symentis.jit;
+
+public class UncommonTrap {
+	static final int CHUNK_SIZE = 1000;
+
+	public static void main(String[] argv) {
+		Object trap = null;
+		for (int i = 0; i < 250; ++i) {
+			for (int j = 0; j < CHUNK_SIZE; ++j) {
+				new Object();
+				if (trap != null) {
+					System.out.println("I am being trapped!");
+					trap = null;
+				}
+			}
+			if (i == 200) {
+				trap = new Object();
+			}
+		}
+	}
+}
