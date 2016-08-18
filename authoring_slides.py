3a29ef1ef2130a71772ca4319fde1be07c8cabd1
jaroslaw.palka <none@none>
2016-08-18 23:02:16 +0200

    added index slide and moved old slides to legacy-slides directory

--- authoring_slides.py
+++ authoring_slides.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from livereload import Server
 import sh
+import webbrowser
 
 SLIDES_SRC = "src/main/asciidoc"
+IMAGES_DIR = "%s/%s" % (SLIDES_SRC,"images")
 SLIDES_TARGET = "target/slides"
 
 def init_slides():
@@ -10,6 +12,7 @@ def init_slides():
     sh.cp("-R", "--remove-destination", "reveal.js", SLIDES_TARGET)
 
 def render_slides():
+    sh.cp("-R","--remove-destination",IMAGES_DIR,SLIDES_TARGET)
     sh.asciidoctor("src/main/asciidoc/*.adoc",
         template_dir="asciidoctor-reveal.js/templates/slim/",
         require="asciidoctor-diagram",
@@ -27,3 +30,5 @@ if __name__=="__main__":
     server = Server()
     server.watch('src/main/asciidoc/*.adoc', render_slides)
     server.serve(root=SLIDES_TARGET)
+
+    webbrowser.open("http://localhost:5500/")
