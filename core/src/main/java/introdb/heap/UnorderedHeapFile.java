e3a09a9fbcbe636c4124e9780ec420331a1de1dd
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2018-10-19 12:50:27 +0200

    added skeleton of heap file class

---
+++ core/src/main/java/introdb/heap/UnorderedHeapFile.java
@@ -0,0 +1,21 @@
+package introdb.heap;
+
+import java.nio.file.Path;
+
+class UnorderedHeapFile {
+
+	UnorderedHeapFile(Path path, int maxNrPages, int pageSize) {
+	}
+
+	void put(Entry entry) {
+	}
+
+	public Object get(String key) {
+		return null;
+	}
+
+	public Object remove(String key) {
+		return null;
+	}
+
+}
