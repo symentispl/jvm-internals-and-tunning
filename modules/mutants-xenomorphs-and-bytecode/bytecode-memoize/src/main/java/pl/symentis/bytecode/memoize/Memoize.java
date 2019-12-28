61501e50a50ed7d8840dcbebbbbb2b05c0f2198f
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2019-12-28 14:23:31 +0100

    removing xenomorphs to switch branch

--- modules/mutants-xenomorphs-and-bytecode/bytecode-memoize/src/main/java/pl/symentis/bytecode/memoize/Memoize.java
+++
@@ -1,9 +0,0 @@
-package pl.symentis.bytecode.memoize;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.METHOD)
-public @interface Memoize {
-
-}
